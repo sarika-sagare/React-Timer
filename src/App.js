ed878270743a70fa6bec767d965cd7d5c1265b44
sarika-sagare <104127875+sarika-sagare@users.noreply.github.com>
2024-08-20 21:59:49 +0530

    added timers function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState,useRef} from 'react';
 
+// start stop pause reset
 function App() {
+  const [time,setTime] = useState(0)
+  const timer = useRef(null)
+
+  const onStart=()=>{
+    if(timer.current){
+      clearInterval(timer.current)
+     }
+     if(time!=0){
+      setTime(0)
+     }
+    timer.current = setInterval(()=>{
+     setTime(prev=>prev+1)
+    },1000)
+  }
+
+  const onStop=()=>{
+   setTime(0)
+   if(timer.current){
+    clearInterval(timer.current)
+   }
+  }
+
+  const onPause=()=>{
+    if(timer.current){
+     clearInterval(timer.current)
+    }
+   }
+   const onReset=()=>{
+      if(time>0){
+        onStart()
+      }
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div style={{justifyContent:'center',alignSelf:'center'}}>
+    <p style={{fontSize:20}}>{time}</p>
+    <button onClick={()=>onStart()}>Start</button>
+    <button onClick={()=>onStop()}>Stop</button>
+    <button onClick={()=>onPause()}>Pause</button>
+    <button onClick={()=>onReset()}>Restart</button>
+   </div>
   );
 }
 
